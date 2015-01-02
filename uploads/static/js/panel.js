fcb66c8623c45da435fe268aa7a9e02be1284ce5
thenitronos <thenitronos@gmail.com>
2015-01-02 21:14:43 +0000

    ajout de panel possibilité de fonctionnement depuis divers templates

--- uploads/static/js/panel.js
+++ uploads/static/js/panel.js
@@ -1,4 +1,8 @@
-$( document ).on( "pagecreate", "#page", function() {
+var templatePanel = document.querySelector('.panel');
+var panel = templatePanel.import.querySelector('#panelContent');
+
+$( document ).on( "pagebeforecreate", "#page", function() {
+    $("#panel").prepend(panel);
     $( document ).on( "swiperight", "#page", function( e ) {
         if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
             if ( e.type === "swiperight" ) {
