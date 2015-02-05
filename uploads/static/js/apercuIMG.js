1a25a02ad24608a2971b8295a8383dc6023eadd1
thenitronos <thenitronos@gmail.com>
2015-02-05 20:01:49 +0000

    test contraste et avancement css

--- uploads/static/js/apercuIMG.js
+++ uploads/static/js/apercuIMG.js
@@ -1,4 +1,17 @@
 var loadFile = function(event) {
     var output = document.getElementById('output');
     output.src = URL.createObjectURL(event.target.files[0]);
-  };
+};
+
+var changecontrast = function(event) {
+    var contraste = document.getElementById('contraste');
+    contraste += ")";
+    var valeurcontraste = "contrast(";
+    valeurcontraste += contraste
+    
+    $("#output").css("-webkit-filter",valeurcontraste);
+};
+
+
+
+
