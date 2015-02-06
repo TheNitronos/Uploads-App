880fe90045b22101faf9dd4c34971f7cc42e9079
thenitronos <thenitronos@gmail.com>
2015-02-06 22:11:22 +0000

    fonctionnement simultané des curseus résolu

--- uploads/static/js/apercuIMG.js
+++ uploads/static/js/apercuIMG.js
@@ -8,21 +8,45 @@ $( document ).on( "pagecreate", function() {
         $( "#contraste" ).on( 'slidestop', function(event) {
             var valeurcontraste = $("#contraste").val();
             var contraste = 'contrast(' + valeurcontraste + '%)';
-            $("#output").css("-webkit-filter", contraste);
+            
+            var valeurluminosite = $("#luminosite").val();
+            var luminosite = 'brightness(' + valeurluminosite + '%)';
+            
+            var valeursaturation = $("#saturation").val();
+            var saturation = 'saturate(' + valeursaturation + '%)';
+            
+            var attribut =  contraste + luminosite +  saturation
+            $("#output").css("-webkit-filter", attribut);
         });
     });
     $( "#luminosite" ).on( 'slidestart', function(event) {
         $( "#luminosite" ).on( 'slidestop', function(event) {
+            var valeurcontraste = $("#contraste").val();
+            var contraste = 'contrast(' + valeurcontraste + '%)';
+            
             var valeurluminosite = $("#luminosite").val();
             var luminosite = 'brightness(' + valeurluminosite + '%)';
-            $("#output").css("-webkit-filter", luminosite);
+            
+            var valeursaturation = $("#saturation").val();
+            var saturation = 'saturate(' + valeursaturation + '%)';
+            
+            var attribut =  contraste + luminosite +  saturation
+            $("#output").css("-webkit-filter", attribut);
         });
     });
     $( "#saturation" ).on( 'slidestart', function(event) {
         $( "#saturation" ).on( 'slidestop', function(event) {
+            var valeurcontraste = $("#contraste").val();
+            var contraste = 'contrast(' + valeurcontraste + '%)';
+            
+            var valeurluminosite = $("#luminosite").val();
+            var luminosite = 'brightness(' + valeurluminosite + '%)';
+            
             var valeursaturation = $("#saturation").val();
             var saturation = 'saturate(' + valeursaturation + '%)';
-            $("#output").css("-webkit-filter", saturation);
+            
+            var attribut =  contraste + luminosite +  saturation
+            $("#output").css("-webkit-filter", attribut);
         });
     });
 });
