32cf17b30e18d61785a27f207267651efc118b07
thenitronos <thenitronos@gmail.com>
2015-02-06 21:34:43 +0000

    curseur avec changements css fonctionnels

--- uploads/static/js/apercuIMG.js
+++ uploads/static/js/apercuIMG.js
@@ -3,9 +3,26 @@ var loadFile = function(event) {
     output.src = URL.createObjectURL(event.target.files[0]);
 };
 
-var changecontrast = function(event) {
-    var valeurcontraste = $("#contraste").val();
-    var contraste = "contrast({})".format(valeurcontraste);
-    
-    $("#output").css("-webkit-filter", contraste);
-};
+$( document ).on( "pagecreate", function() {
+    $( "#contraste" ).on( 'slidestart', function(event) {
+        $( "#contraste" ).on( 'slidestop', function(event) {
+            var valeurcontraste = $("#contraste").val();
+            var contraste = 'contrast(' + valeurcontraste + '%)';
+            $("#output").css("-webkit-filter", contraste);
+        });
+    });
+    $( "#luminosite" ).on( 'slidestart', function(event) {
+        $( "#luminosite" ).on( 'slidestop', function(event) {
+            var valeurluminosite = $("#luminosite").val();
+            var luminosite = 'brightness(' + valeurluminosite + '%)';
+            $("#output").css("-webkit-filter", luminosite);
+        });
+    });
+    $( "#saturation" ).on( 'slidestart', function(event) {
+        $( "#saturation" ).on( 'slidestop', function(event) {
+            var valeursaturation = $("#saturation").val();
+            var saturation = 'saturate(' + valeursaturation + '%)';
+            $("#output").css("-webkit-filter", saturation);
+        });
+    });
+});
