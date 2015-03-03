cc5b87994432589a1a74ab1e41c2264c45caa290
thenitronos <thenitronos@gmail.com>
2015-03-03 18:26:32 +0000

    suppresion des instances d'images

--- uploads/models.py
+++ uploads/models.py
@@ -1,24 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class BaseProfile(models.Model):
-    user = models.OneToOneField(User)
-    avatar = models.ImageField(null=True, blank=True, upload_to="avatars/")
-    theme = models.CharField(max_length=1)
-    
-    class Meta:
-        abstract = True
-
-class Teacher(BaseProfile):
-
-    def __str__(self):
-        return "Professeur {0}".format(self.user.username)
-
-class Student(BaseProfile):
-
-    def __str__(self):
-        return "Etudiant {0}".format(self.user.username)
-
 class Picture(models.Model):
     #ImageField pour que l'image soit enregistrée comme une image
     image = models.ImageField(upload_to="uploadedImages")
@@ -40,7 +22,7 @@ class Picture(models.Model):
     luminosite = models.DecimalField(decimal_places=1, max_digits=2, default=0)
     
     #relations de l'image
-    proprietaire = models.OneToOneField(Student, null=True)
+    #proprietaire = models.OneToOneField(Student, null=True)
     
     #date de l'upload
     date = models.DateField(auto_now_add=True)
