8a48c065d0d00d93e9f533b2929ff09043064c35
thenitronos <thenitronos@gmail.com>
2015-03-04 19:42:08 +0000

    essai compression

--- uploads/static/js/apercuIMG.js
+++ uploads/static/js/apercuIMG.js
@@ -2,8 +2,8 @@ var loadFile = function(input) {
     var output = document.getElementById('output');
     var srcImg = URL.createObjectURL(input.files[0]);
     output.src = srcImg;
-    var hauteur = $( window ).height()*0.6;
-    $("#output").css("max-height", hauteur)
+    var hauteurpage = $( window ).height()*0.6;
+    $("#output").css("max-height", hauteurpage)
 };
 
 $( document ).on( "pagecreate", function() {
