b6ab1791490c890a24a0084737f1631a71a8a0f5
thenitronos <thenitronos@gmail.com>
2015-03-12 16:36:20 +0000

    avancement template details

---
+++ uploads/static/js/navbar.js
@@ -0,0 +1,13 @@
+$(document).delegate('.ui-navbar a', 'click', function () {
+    $("#btnmodifier").removeClass("ui-btn-active");
+    $("#btnsupprimer").removeClass("ui-btn-active");
+    $("#btnimage").removeClass("ui-btn-active");
+    
+    $(this).addClass('ui-btn-active');
+    
+    $("#image").hide();
+    $("#modifier").hide();
+    $("#supprimer").hide();
+    
+    $('#' + $(this).attr("data-href")).show();
+});
