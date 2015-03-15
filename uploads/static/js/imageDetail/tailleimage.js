01fd3fe1ced8494921bbdedb9afc6fd88c523e6b
thenitronos <thenitronos@gmail.com>
2015-03-15 22:26:16 +0000

    correction bug taille image

--- uploads/static/js/imageDetail/tailleimage.js
+++ uploads/static/js/imageDetail/tailleimage.js
@@ -1,4 +1,4 @@
-( document ).on( "pagecreate", function() {
+$( document ).on( "pagecreate", function() {
     $( "#largeur" ).on( 'slidestop', function() {
         $("#imagePrincipale").css("width", $("#largeur").val()+"%");
    });
