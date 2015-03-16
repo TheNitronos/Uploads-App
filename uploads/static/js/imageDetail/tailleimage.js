6769c69219f18c56bb6f590276ba0196968df5f6
thenitronos <thenitronos@gmail.com>
2015-03-16 20:56:56 +0000

    resolution bug taille image

--- uploads/static/js/imageDetail/tailleimage.js
+++ uploads/static/js/imageDetail/tailleimage.js
@@ -1,4 +1,5 @@
 $( document ).on( "pagecreate", function() {
+    $("#imagePrincipale").css("width", "50%");
     $( "#largeur" ).on( 'slidestop', function() {
         $("#imagePrincipale").css("width", $("#largeur").val()+"%");
    });
