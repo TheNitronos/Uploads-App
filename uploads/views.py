a5e09834bec301723eb20b3b9a1990ca64de2d3e
thenitronos <thenitronos@gmail.com>
2015-03-19 16:09:33 +0000

    views

--- uploads/views.py
+++ uploads/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from uploads.forms import UploadForm, ModifyForm, changeTheme
-from uploads.models import Picture
+from uploads.forms import *
+from uploads.models import *
 
 #affichage du code de base pour une page en jQM
 def base(request):
@@ -70,4 +70,61 @@ def modify(request, imageId):
         else:
             return redirect('uploads:uploaded')
             
+
+def connexion(request):
+    erreur = False
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+            else:
+                erreur = True
+    else:
+        form = LoginForm()
+        
+    return render(request, "mobile_uploads/login.html", locals())
+    
+def deconnexion(request):
+    logout(request)
+    return redirect('uploads:connexion')
+    
+    
+def register(request):
+    if request.method == "POST":
+        registerform = RegisterForm(data=request.POST)
+        
+        if registerform.is_valid():
+            username = registerform.cleaned_data["username"]
+            password = registerform.cleaned_data["password"]
+            mail = registerform.cleaned_data["mail"]
             
+            try:
+                User.objects.get(username=username)
+            except User.DoesNotExist:
+                user = User.objects.create_user(username, mail, password)
+                user.save()
+                
+                account_model = None # Modèle à instancier pour créer le compte
+                
+                if registerform.cleaned_data["account_type"] == "student":
+                    account_model = Student # Le modèle à utiliser est Student
+                    
+                elif registerform.cleaned_data["account_type"] == "teacher":
+                    account_model = Teacher # Le modèle à utiliser est Teacher
+                    
+                account = account_model() # Instanciation du modèle
+                account.user = user # Liaison au compte user
+                account.save()
+                
+                return redirect('uploads:connexion')
+            except Exception as e:
+                return HttpResponse("Erreur non gérée : {}".format(str(e)))
+
+    else:
+        registerform = RegisterForm()
+        
+    return render(request, "mobile_uploads/register.html", {'registerform' : registerform})
