f79457e9b2580d02be59f2ce28efb5f5542694ac
thenitronos <thenitronos@gmail.com>
2015-03-24 13:45:32 +0000

    app finale

--- projet_daniel/settings.py
+++ projet_daniel/settings.py
@@ -56,8 +56,6 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_mobile.middleware.MobileDetectionMiddleware',
-    'django_mobile.middleware.SetFlavourMiddleware',
 )
 
 ROOT_URLCONF = 'projet_daniel.urls'
